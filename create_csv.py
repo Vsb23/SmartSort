4d605406335b801c6d370de822ee93a5c0462f87
SimoneColumpsi <95437393+SimoneColumpsi@users.noreply.github.com>
2025-09-23 14:37:10 +0200

    modificata gestione del titolo mancante e dell'autore mancante

--- create_csv.py
+++ create_csv.py
@@ -11,10 +11,17 @@ def extract_metadata_from_pdf(pdf_path):
             pdf_reader = PyPDF2.PdfReader(file)
             metadata = pdf_reader.metadata
             
-            title = metadata.get('/Title', os.path.basename(pdf_path)) if metadata else os.path.basename(pdf_path)
+            title = metadata.get('/Title', os.path.basename(pdf_path)) if metadata else None
+            # Se titolo mancante, usa nome file o altra stringa significativa
+            if not title or title.strip() == '':
+                print(f"Erorre con il titolo del file {os.path.basename(pdf_path)}")
+                title = os.path.splitext(os.path.basename(pdf_path))[0]
             author = metadata.get('/Author', 'Unknown') if metadata else "Unknown"
             if "," in author:
                  author = author.replace(",", "; ") # Sostituisce le virgole con punti e virgola
+            # Se autore mancante, inserisci Unknown
+            if not author or author.strip() == '':
+                print(f"Erorre con l'autore del file {os.path.basename(pdf_path)}")
 
             # Usa metadata per ottenere la data di creazione
             creation_date = metadata.get('/CreationDate') if metadata else None
@@ -30,7 +37,7 @@ def extract_metadata_from_pdf(pdf_path):
             
     except Exception as e:
         print(f"Errore nel processare {pdf_path}: {str(e)}")
-        return os.path.basename(pdf_path), "Unknown", "Unknown", "Unknown"
+        return os.path.splitext(os.path.basename(pdf_path))[0], "Unknown", "Unknown", "Scienza"
 def write_to_csv(data, output_csv):
     
     with open(output_csv, 'w', newline='', encoding='utf-8') as csvfile:
@@ -38,10 +45,9 @@ def write_to_csv(data, output_csv):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for entry in data:
-            titolo, autore, anno, category = entry.split(',')
-            if not titolo or not autore or not anno or not category:
-                print(f"Skipping incomplete entry: {entry}")
-                continue
+            titolo, autore, anno, category = entry
+          
+
             writer.writerow({'titolo': titolo, 'autore': autore, 'anno': anno, 'category': category})
 
 
@@ -50,8 +56,8 @@ all_data = []
 for f in pdfpath:
     if f.lower().endswith('.pdf'):
         full_path = os.path.join("./References", f)
-        titolo, autore, anno, category = extract_metadata_from_pdf(full_path)
-        all_data.append(f"{titolo},{autore},{anno},{category}")
+        metadata_tuple = extract_metadata_from_pdf(full_path)
+        all_data.append(metadata_tuple)
     else:
         print(f"Saltando file non-PDF: {f}")
         
